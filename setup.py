b67ca76df55d5e1e5406900e13727cebbfb965c2
Alexander Kashev <alexander.kashev@math.unibe.ch>
2018-06-11 16:26:49 +0200

    Update setup.py and README for long description

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
 from setuptools import setup
 from singularity_pipeline import __version__
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='singularity-pipeline',
     version=__version__,
     description='A runner script for pipelines using Singularity containers',
-    url='https://github.com/kav2k/singularity-pipeline',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/kav2k/singularity-pipeline/',
     author='Alexander Kashev',
     author_email='alexander.kashev@math.unibe.ch',
     license='MIT',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3'
+        'Programming Language :: Python :: 3',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux"
     ],
     keywords='singularity container runner',
     packages=['singularity_pipeline'],
